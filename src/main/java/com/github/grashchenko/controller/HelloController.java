ad0b4859df5f55c1c0a5a982f90c36fbbcaf3ce8
Vladi207 <graschenko207@mail.ru>
2022-10-31 22:09:15 +0300

    refactor

--- src/main/java/com/github/grashchenko/controller/HelloController.java
+++ src/main/java/com/github/grashchenko/controller/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
 
     @GetMapping("/hello")
-    public String seyHelloswje() {
+    public String seyHello() {
         return "helloWorld";
     }
 }
